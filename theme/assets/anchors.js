c18a119f2b3133bbb15028a03b627964725b361e
Joseph Hager <ajhager@gmail.com>
2015-08-13 07:49:23 -0500

    Only generate anchors for headings h1, h2, h3

--- theme/assets/anchors.js
+++ theme/assets/anchors.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function () {
-	$('section > :header').each(function(i, elem) {
+	$('section > h1,h2,h3').each(function(i, elem) {
 		var header = $(elem);
 		var id = header.attr('id');
 		header.append('<a name="' + id + '" class="anchor" ' 
